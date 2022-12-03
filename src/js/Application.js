786b6b16cb1446b7df6fd348e2a393faaefba255
Martin <mdsugarev@gmail.com>
2022-12-03 23:26:39 +0200

    another one

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,5 +1,5 @@
 import EventEmitter from "eventemitter3";
-import Beat from "./Beat";
+import {Beat} from "./Beat";
 
 export default class Application extends EventEmitter {
   static get events() {
@@ -11,16 +11,16 @@ export default class Application extends EventEmitter {
   constructor() {
     
     super();
-
+    
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
-
+    
     const message = document.createElement("div");
     message.classList.add("message");
     message.innerText = "Ah";
-
-    document.querySelector(".main").appendChild(message);
+    
     this._beat = new Beat();
+    document.querySelector(".main").appendChild(message);
      this._beat.addListener(this._beat.events.BIT, function(){
       console.log("beat");
     })
